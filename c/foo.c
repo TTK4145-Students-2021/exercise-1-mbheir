69bbb6d3ae89d69c3afbb5071b7af3b49bc11221
Martin Heir <mbheir@gmail.com>
2021-02-02 13:31:57 +0100

    Done

--- c/foo.c
+++ c/foo.c
@@ -1,24 +1,31 @@
-// Compile with `gcc foo.c -std=c99 -lpthread`, or use the makefile
+c// Compile with `gcc foo.c -std=c99 -lpthread`, or use the makefile
 
 #include <pthread.h>
 #include <stdio.h>
 
 int i = 0;
+int number_of_executions = 1000000;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for (int inc = 0; inc<number_of_executions; ++inc) {
+        ++i;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
-    // TODO: decrement i 1_000_000 times
+    for (int inc = 0; inc<number_of_executions; ++inc) {
+        --i;
+    }
     return NULL;
 }
 
 
 int main(){
-    // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
@@ -26,5 +33,6 @@ int main(){
     pthread_join(decrementingThread, NULL);
     
     printf("The magic number is: %d\n", i);
+    printf("%lu\n", sizeof(int));
     return 0;
 }
--- go/foo.go
+++ go/foo.go
@@ -3,29 +3,46 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	"sync"
+	// "time"
 )
 
 var i = 0
+var number_of_iterations = 1000000
 
-func incrementing() {
-    //TODO: increment i 1000000 times
+func incrementing(wg sync.WaitGroup) {
+	for inc := 0; inc < number_of_iterations; inc++ {
+		i++
+		Println(i)
+	}
+	wg.Done()
+
 }
 
-func decrementing() {
-    //TODO: decrement i 1000000 times
+func decrementing(wg sync.WaitGroup) {
+	for inc := 0; inc < number_of_iterations; inc++ {
+		i--
+	}
+	wg.Done()
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
+	runtime.GOMAXPROCS(runtime.NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
+	// Try doing the exercise both with and without it!
 
-    // TODO: Spawn both functions as goroutines
-	
-    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
-    // We'll come back to using channels in Exercise 2. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
-    Println("The magic number is:", i)
+	// TODO: Spawn both functions as goroutines
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go incrementing(wg)
+
+	go decrementing(wg)
+
+	wg.Wait()
+
+	// We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+	// We'll come back to using channels in Exercise 2. For now: Sleep.
+	Println("The magic number is:", i)
 }
